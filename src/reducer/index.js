81651d31cd87e5f330425cd26e8fd1a0cc242ec1
Sone <sthaya@gmail.com>
2020-07-04 21:08:48 -0700

    set up combine reducers

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,2 +1,17 @@
 import { combineReducers } from 'redux'
 
+import { loginReducer } from './loginReducer';
+import { registerReducer } from './signUpReducer'
+import { getItemsReducer } from './getItemsReducer'
+import { addItemReducer } from './addItemReducer'
+import { deleteItemReducer } from './deleteItemReducer'
+import { updateItemReducer } from './updateItemReducer'
+
+export default combineReducers({
+  loginReducer,
+  registerReducer,
+  getItemsReducer,
+  addItemReducer,
+  deleteItemReducer,
+  updateItemReducer,
+})
