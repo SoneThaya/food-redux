012ca60d0423e835a544c201f838a1adcb67e920
Sone <sthaya@gmail.com>
2020-07-05 15:35:51 -0700

    created home component, completed add item component

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom"
 import InputLabel from "@material-ui/core/InputLabel"
 import TextField from "@material-ui/core/TextField"
 
+import { Styles } from './Styles';
+
 import { connect } from 'react-redux';
 import { addItem } from '../action/addItem'
 
@@ -34,10 +36,101 @@ const AddItem = (props) => {
   }
 
   return (
-    <div>
-      
-    </div>
+    <Styles>
+			<div>
+				<div className="tabs-container">
+					<Link
+						className="tab"
+						href="https://bw1-marketing-page.now.sh/"
+					>
+						Home
+					</Link>
+					<Link className="tab" to="/user-recipes">
+						My Recipes
+					</Link>
+					<Link className="tab active" to="/add-recipe">
+						Add Recipe
+					</Link>
+				</div>
+				<div className="entry-container">
+					<h2>Add an Item</h2>
+          <form onSubmit={handleSubmit}>
+            
+            <br />
+            
+						<TextField
+							required
+							label="title"
+							id="title"
+							name="title"
+							value={itemToAdd.title}
+							onChange={onChange}
+            />
+            
+            <br />
+            
+						<TextField
+							required
+							label="description"
+							id="description"
+							name="description"
+							value={itemToAdd.description}
+							onChange={onChange}
+            />
+            
+            <br />
+
+            <TextField
+              required
+              label="category"
+              id="category"
+              name="category"
+              value={recipeToAdd.category}
+              onChange={onChange}
+            />
+						
+            <br />
+
+            <TextField
+							required
+							label="price"
+							id="price"
+							name="price"
+							value={itemToAdd.price}
+							onChange={onChange}
+						/>
+    
+						<br />
+
+            <TextField
+							required
+							label="itemImage"
+							id="itemImage"
+							name="itemImage"
+							value={itemToAdd.itemImage}
+							onChange={onChange}
+            />
+            
+            <br />
+            
+						<Button
+							type="submit"
+							variant="contained"
+							color="primary"
+						>
+							Submit
+						</Button>
+					</form>
+				</div>
+			</div>
+		</Styles>
   )
 }
 
-export default AddItem
+const mapStateToProps = (state) => {
+  return {
+    addItem: state.addItem,
+  }
+}
+
+export default connect(mapStateToProps, { addItem })(AddItem)
---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+import { Link } from "react-router-dom"
+import { Styles } from "./Styles"
+
+const Home = () => {
+	return (
+		<Styles>
+			<div className="tabs-container">
+				<Link className="tab" to="/all-recipes">
+					Home
+				</Link>
+				<Link className="tab" to="/user-recipes">
+					My Recipes
+				</Link>
+				<Link className="tab" to="/add-recipe">
+					Add Recipe
+				</Link>
+			</div>
+		</Styles>
+	)
+}
+export default Home
