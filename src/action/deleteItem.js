52ea2ec3bd06f806690204ef73e8a45687e01b15
Sone <sthaya@gmail.com>
2020-07-04 17:33:58 -0700

    cleaned up files and added delete item action

---
+++ src/action/deleteItem.js
@@ -0,0 +1,22 @@
+import {
+  DELETE_ITEM_START,
+  DELETE_ITEM_SUCCESS,
+  DELETE_ITEM_FAILURE,
+} from './index';
+
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const deleteItem = (id, history) => (dispatch) => {
+  axiosWithAuth()
+    .delete(`/menu/${id}`)
+    .then(res => {
+      console.log(res)
+      dispatch({ type: DELETE_ITEM_START })
+      dispatch({ type: DELETE_ITEM_SUCCESS, payload: id })
+      history.push(`/menu`)
+    })
+    .catch(err => {
+      console.log(err)
+      dispatch({ type: DELETE_ITEM_FAILURE, payload: err })
+    })
+}
